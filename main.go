b8ea7fc65cd5a7e50b47414f46e28f2acf1ec513
max <max@trafficstars.com>
2023-11-25 21:28:16 +0200

    Add /partners and /image

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -8,6 +9,7 @@ import (
 	"os"
 	"strconv"
 
+	"golang.org/x/sync/errgroup"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -80,6 +82,22 @@ select category.id, category.name, headline_banner.url
 from category
 left join headline_banner on category.id = headline_banner.category_id
 where category.parent_id = ?::int;`
+
+	// language=PostgreSQL
+	partnersSQL = `
+select id, headline, description, location[0] as latitude, location[1] as longitude, price_level
+from partner;`
+
+	// language=PostgreSQL
+	bannerURLsSQL = `
+select url, partner_id 
+from headline_banner;`
+
+	// language=PostgreSQL
+	bannerImagesSQL = `
+select image 
+from headline_banner 
+where url = $1`
 )
 
 type Storage struct {
@@ -105,6 +123,60 @@ func (s *Storage) GetCategories(parentId int) (*[]Category, error) {
 		return nil, fmt.Errorf("GetCategories: %w", err)
 	}
 	return &categories, nil
+}
+
+type Partner struct {
+	Id          int      `json:"id" gorm:"id"`
+	Headline    string   `json:"headline" gorm:"headline"`
+	Description string   `json:"description" gorm:"description"`
+	Latitude    float64  `json:"latitude" gorm:"latitude"`
+	Longitude   float64  `json:"longitude" gorm:"longitude"`
+	PriceLevel  int8     `json:"price_level" gorm:"price_level"`
+	BannerURLs  []string `json:"urls"`
+}
+
+func (s *Storage) GetPartners() ([]Partner, error) {
+	var partners []Partner
+	if err := s.db.Raw(partnersSQL).Scan(&partners).Error; err != nil {
+		return nil, fmt.Errorf("GetPartners: %w", err)
+	}
+	return partners, nil
+}
+
+type HeadlineBanner struct {
+	URL        string `json:"url" gorm:"url"`
+	PartnerID  int    `json:"partner_id" gorm:"partner_id"`
+	ActionID   int    `json:"action_id" gorm:"action_id"`
+	CategoryID int    `json:"category_id" gorm:"category_id"`
+	Image      []byte `json:"image" gorm:"image"`
+}
+
+func (s *Storage) GetBannerImageByURL(url string) ([]byte, error) {
+	conn, err := s.db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	var image []byte
+	if err = conn.QueryRow(bannerImagesSQL, url).Scan(&image); err != nil {
+		return nil, fmt.Errorf("GetBannerURLs: %w", err)
+	}
+
+	return image, nil
+}
+
+func (s *Storage) GetBannerURLs() (map[int][]string, error) {
+	var banners []HeadlineBanner
+	if err := s.db.Raw(bannerURLsSQL).Scan(&banners).Error; err != nil {
+		return nil, fmt.Errorf("GetBannerURLs: %w", err)
+	}
+
+	bannersMap := make(map[int][]string, len(banners))
+	for _, b := range banners {
+		bannersMap[b.PartnerID] = append(bannersMap[b.PartnerID], b.URL)
+	}
+
+	return bannersMap, nil
 }
 
 type Application struct {
@@ -137,6 +209,51 @@ func (a *Application) GetCategories(w http.ResponseWriter, r *http.Request) {
 	enc.Encode(categories)
 }
 
+func (a *Application) GetPartners(w http.ResponseWriter, _ *http.Request) {
+	g, _ := errgroup.WithContext(context.Background())
+	var partners []Partner
+	g.Go(func() (err error) {
+		partners, err = a.storage.GetPartners()
+		return err
+	})
+
+	var banners map[int][]string
+	g.Go(func() (err error) {
+		banners, err = a.storage.GetBannerURLs() // TODO: use normal join, not select * from headlie_banners;
+		return err
+	})
+
+	if err := g.Wait(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if banners != nil {
+		for i := range partners {
+			partners[i].BannerURLs = banners[partners[i].Id]
+		}
+	}
+
+	enc := json.NewEncoder(w)
+	enc.SetEscapeHTML(false)
+	enc.Encode(partners)
+}
+
+func (a *Application) GetImage(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query().Get("url")
+
+	image, err := a.storage.GetBannerImageByURL(url)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.Write(image)
+}
+
 func main() {
 	// POSTGRESQL="host=localhost port=5432 user=postgres password=first_iteration dbname=default sslmode=disable"
 	db, err := gorm.Open(postgres.Open(os.Getenv("POSTGRESQL")), &gorm.Config{})
@@ -152,6 +269,8 @@ func main() {
 	app := Application{storage: storage}
 	http.HandleFunc("/healthcheck", app.HealthCheck)
 	http.HandleFunc("/categories", app.GetCategories)
+	http.HandleFunc("/partners", app.GetPartners)
+	http.HandleFunc("/image", app.GetImage)
 
 	// LISTEN=:8080
 	if err := http.ListenAndServe(os.Getenv("LISTEN"), nil); err != nil {
